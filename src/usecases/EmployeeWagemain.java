00dba9f4512f6f7f53a946a6ad704cbf354488ce
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-10 14:57:33 +0530

    part time employee is added

--- src/usecases/EmployeeWagemain.java
+++ src/usecases/EmployeeWagemain.java
@@ -3,16 +3,18 @@ package usecases;
 public class EmployeeWagemain {
     public static void main(String[] args) {
         int isFullTime = 1;
+        int isPartTime = 2;
         int empRatePerhr = 20;
         int empHr = 0;
         int empWage=0;
-        double randomCheck = Math.floor(Math.random() * 10) % 2;
+        double randomCheck = Math.floor(Math.random() * 10) % 3;
 
         if (randomCheck == isFullTime)
             empHr = 8;
 
+        else if (randomCheck == isPartTime)
+            empHr=4;
         else
-
             empHr = 0;
 
 
