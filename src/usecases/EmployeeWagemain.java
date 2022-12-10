a28f55a72821366ed14ebe1cdb10e74dd19b1e15
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-10 15:32:33 +0530

    calculate wage for month

--- src/usecases/EmployeeWagemain.java
+++ src/usecases/EmployeeWagemain.java
@@ -3,26 +3,40 @@ package usecases;
 public class EmployeeWagemain {
     public static final int isFullTime = 1;
     public static final int isPartTime = 2;
-    public static void main(String[] args) {
 
+    public static final int noOfWorkingDays=20;
+
+    public static void main(String[] args) {
+        int isFullTime=0;
         int empRatePerhr = 20;
         int empHr = 0;
         int empWage = 0;
-        int randomCheck = (int) Math.floor(Math.random() * 10) % 3;
+        int totalEmpWage = 0;
+        int noOfWorkingDays = 20;
 
-        switch (randomCheck) {
-            case  isFullTime:
+        for (int day = 0 ; day < noOfWorkingDays;day++)
+        {
+            int randomCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+            if(isFullTime == randomCheck)
+            {
                 empHr = 8;
-                break;
-            case isPartTime:
+            } else if (isPartTime == randomCheck) {
                 empHr = 4;
-                break;
-            default:
+            }else {
                 empHr=0;
+            }
+            empWage = empHr * empRatePerhr;
+            System.out.println("EmpWage is for day "+ day +" "  + empWage);
+            totalEmpWage+=empWage;
+
 
         }
-        empWage=empHr*empRatePerhr;
-        System.out.println("EmpWage is :" + empWage);
+
+        System.out.println("total EmpWageis :"+totalEmpWage);
+
+
+
     }
         }
 
